5b644d0891d67588a5757e90c4bf5a7fde7d191e
Leonardo Lazzaro <lazzaroleonardo@mindpixel.com.ar>
2011-09-16 21:43:29 -0300

    unfinished run script

---
+++ run_me.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+from optparse import OptionParser
+
+from lsa import main
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('-f', '--file', action='open file',
+                type='string', dest='file', help='file top open')
+
+    options, args = parser.parse_args()
+
+    if not options.file:
+        print("Filename is required!")
+        parser.print_help()
+        sys.exit(1)
